8ac4b6e771d878c34b5eec6b05076376bbc4f1f8
Charlie Bayne <baynec2@gmail.com>
2022-07-08 13:43:50 -0700

    added function to calculate Pi score

---
+++ R/calc_pi_score.R
@@ -0,0 +1,15 @@
+#' calc_pi_score
+#'
+#' This function returns the pi score
+#'
+#' @param pval : p value
+#' @param Log2FC : log 2 fold change
+#'
+#' @return The pi score
+#' @export
+#'
+#' @examples
+calc_pi_score = function(pval,Log2FC){
+  output = -log10(pval) * abs(Log2FC)
+  return(output)
+}

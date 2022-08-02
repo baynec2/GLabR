f6ad332a9bffe6d5429b923c60c382200b648c49
Charlie Bayne <baynec2@gmail.com>
2022-08-02 10:14:15 -0700

    fixed bug causing some # of iterations not to work. Also added progress bar to monitor whether it is working.

--- R/annotate_proteins.R
+++ R/annotate_proteins.R
@@ -15,35 +15,45 @@ annotate_proteins = function(protein_list,columns = NULL){
   # Adding OR term to do multiple proteins per request
 
   if(length(protein_list) > 200){
-  master_list = paste0(protein_list,"+OR+")
-  #Determining how many times we will have to iterate to get all accessions on initial list. 300 seems to be a good number per batch
-  num_iterations = floor(length(master_list)/200)
-  #calculating the end of each iteration
-  end = c((1:num_iterations) * 200,length(master_list))
-  #calculating the start of each iteration
-  start = c((end - 199)[1:length(end) - 1],end[length(end)-1] +1)
+    master_list = paste0(protein_list,"+OR+")
+    #Determining how many times we will have to iterate to get all accessions on initial list. 300 seems to be a good number per batch
+    num_iterations = floor(length(master_list)/200)
+    #calculating the end of each iteration
+    end = c((1:num_iterations) * 200)
+    #calculating the start of each iteration
+    start = c((end - 199)[1:length(end) - 1],end[length(end)-1] +1)
   }else{
     master_list = paste0(protein_list,"+OR+")
     start = 1
     end = length(protein_list)
-}
+  }
+
+  #Setting progress bar
+  pb = txtProgressBar(min = 0,
+                      max = num_iterations,
+                      style = 3,
+                      width = 50,
+                      char = "=")
+
   #Initializing a data frame to hold results
   output = data.frame()
-    for( i in 1:length(start)){
-      #Iterating through the batches
-      temp_list = master_list[start[i]:end[i]]
-      #Getting rid of last OR term
-      temp_list[length(temp_list)] = gsub("\\+OR\\+","",temp_list[length(temp_list)])
-      temp_string_list =  paste(temp_list, sep="", collapse="")
-      # Defining columns that we want to return. Defaults to default columns.
-      if(is.null(columns)){
-        request = paste0(baseUrl,temp_string_list,"&format=tsv","&size=",length(temp_list))
-      }else{
-        request = paste0(baseUrl,temp_string_list,"&format=tsv","&fields=",columns,"&size=",length(temp_list))
-      }
-      returned_data = read.csv(request, header = TRUE, sep = '\t')
-      output = rbind(output,returned_data)
+  for( i in 1:length(start)){
+    #Iterating through the batches
+    temp_list = master_list[start[i]:end[i]]
+    #Getting rid of last OR term
+    temp_list[length(temp_list)] = gsub("\\+OR\\+","",temp_list[length(temp_list)])
+    temp_string_list =  paste(temp_list, sep="", collapse="")
+    # Defining columns that we want to return. Defaults to default columns.
+    if(is.null(columns)){
+      request = paste0(baseUrl,temp_string_list,"&format=tsv","&size=",length(temp_list))
+    }else{
+      request = paste0(baseUrl,temp_string_list,"&format=tsv","&fields=",columns,"&size=",length(temp_list))
     }
+    returned_data = read.csv(request, header = TRUE, sep = '\t')
+    output = rbind(output,returned_data)
+    #updating progress bar
+    setTxtProgressBar(pb, i)
+  }
 
   output = tibble::as_tibble(output)
 

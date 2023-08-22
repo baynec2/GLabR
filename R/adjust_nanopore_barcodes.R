55f815c9778faeb3558752b8122ddd897e6b999b
Charlie Bayne <baynec2@gmail.com>
2023-08-22 16:43:24 -0700

    updating adjust nanopore barcodes to have option to keep the same final volume or not.

--- R/adjust_nanopore_barcodes.R
+++ R/adjust_nanopore_barcodes.R
@@ -7,7 +7,7 @@
 #' @param barcodes this is the names of the barcodes.
 #' @param proportion_of_bases this is the proportion of base pairs that belong to each barcode. Note this should be in the same order as the names of the barcodes.
 #' @param volume_of_each_barcode_added this is the volume that the protocol indicates each barcode to be added at. Note this should be in the same order as the names of the barcodes.
-#'
+#' @param same_total_volume do you want the same total volume? Or do you want a different total volume? Different total volume will have the same amount of total DNA while same total volume will be even but scaled down.
 #' @return a tibble with intermediate calculations and final volume to be added.
 #' @export
 #'
@@ -17,8 +17,8 @@
 #'barcodes = test$barcode
 #'proportion_of_bases = test$proportion_of_bases
 #'volume_of_each_barcode_added = 1.25
-#'adjust_nanopore_barcodes(barcodes,proportion_of_bases,volume_of_each_barcode_added = 1.25)
-adjust_nanopore_barcodes = function(barcodes,proportion_of_bases,volume_of_each_barcode_added = 1.25 ){
+#'adjust_nanopore_barcodes(barcodes,proportion_of_bases,volume_of_each_barcode_added = 1.25,same_total_volume == TRUE)
+adjust_nanopore_barcodes = function(barcodes,proportion_of_bases,volume_of_each_barcode_added = 1.25,same_total_volume = TRUE){
 
 # What proportion would each barcode be if everything worked such that each barcode was evenly distributed.
 expected_proportion = 1/ length(barcodes)
@@ -27,18 +27,23 @@ expected_proportion = 1/ length(barcodes)
 adjustments = expected_proportion / proportion_of_bases
 
 # how would this translate into the amount of volume that needs to be added?
-#this isn't correct because there is less molecules than intended with this solution. This gives the same volume at the end as what the original protocol calls for.
-# You could also argue that you don't need to do that, you could just use a greater volume since there is an ampure cleanup.
-# factor to multiply by adjustment = total_volume / sum of proportions
-# adjusted volume = factor to multiply by adjustment * adjustment
 
-total_volume = volume_of_each_barcode_added * length(barcodes)
-sum_of_proportions = sum(adjustments)
 
-x = (total_volume / sum_of_proportions)
+if(same_total_volume == TRUE){
+  # This gives the same volume at the end as what the original protocol calls for.
+  # You could also argue that you don't need to do that, you could just use a greater volume since there is an ampure cleanup.
+  # factor to multiply by adjustment = total_volume / sum of proportions
+  # adjusted volume = factor to multiply by adjustment * adjustment
+
+  total_volume = volume_of_each_barcode_added * length(barcodes)
+  sum_of_proportions = sum(adjustments)
+
+  x = (total_volume / sum_of_proportions)
 
 adjusted_volume = x * adjustments
-
+}else{
+  adjusted_volume = adjustments * volume_of_each_barcode_added
+}
 df = tibble::tibble(barcodes = barcodes,
                     proportion_of_bases = proportion_of_bases,
                     expected_proportion = expected_proportion,
